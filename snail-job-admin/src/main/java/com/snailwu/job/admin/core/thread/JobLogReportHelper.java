d2354cfcd13ac7d8d9b12650dafd990b74e9caf3
WuQinglong <snail.wu@foxmail.com>
2020-07-21 20:13:20 +0800

    Commit

---
+++ snail-job-admin/src/main/java/com/snailwu/job/admin/core/thread/JobLogReportHelper.java
@@ -0,0 +1,39 @@
+package com.snailwu.job.admin.core.thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author 吴庆龙
+ * @date 2020/7/21 5:05 下午
+ */
+public class JobLogReportHelper {
+    public static final Logger log = LoggerFactory.getLogger(JobLogReportHelper.class);
+
+    private static Thread logReportThread;
+    private static volatile boolean toStop = false;
+
+    public void start() {
+        logReportThread = new Thread(() -> {
+            while (!toStop) {
+                for (int i = 0; i < 3; i++) {
+
+
+                }
+            }
+        });
+        logReportThread.setDaemon(true);
+        logReportThread.setName("JobLogReportThread");
+        logReportThread.start();
+    }
+
+    public void stop() {
+        logReportThread.interrupt();
+        try {
+            logReportThread.join();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+}
