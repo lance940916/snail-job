881ea5ce769d99451f79f53484bdb0feeb84c6fe
WuQinglong <snail.wu@foxmail.com>
2020-12-01 10:01:52 +0800

    Commit

--- snail-job-core/src/main/java/com/snailwu/job/core/constants/RegistryConstant.java
+++ snail-job-core/src/main/java/com/snailwu/job/core/constants/RegistryConstant.java
@@ -6,19 +6,19 @@ package com.snailwu.job.core.constants;
  * @author 吴庆龙
  * @date 2020/5/25 4:16 下午
  */
-public class RegistryConstant {
+public interface RegistryConstant {
 
     /**
      * 心跳间隔时间
      * 单位：秒
      */
-    public static final int BEAT_TIME = 30;
+    int BEAT_TIME = 30;
 
     /**
      * 主机死亡时间
      * 规定 心跳三次失败，则认定主机死亡
      * 单位：秒
      */
-    public static final int DEAD_TIME = BEAT_TIME * 3;
+    int DEAD_TIME = BEAT_TIME * 3;
 
 }
